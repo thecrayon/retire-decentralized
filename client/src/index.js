2f2fd2d703cf488d409cf2341fbdef88f943aaf3
Billy Bund <billybund@Billys-MacBook-Pro-2.local>
2023-03-18 21:37:08 -0600

    removed syncfusion

--- client/src/index.js
+++ client/src/index.js
@@ -1,69 +1,68 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { registerLicense } from '@syncfusion/ej2-base';
 import {
-  EthereumClient, modalConnectors,
-  walletConnectProvider
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
 } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { mainnet, avalancheFuji } from "wagmi/chains";
 
-import App from './App';
+import App from "./App";
 import { theme } from "./chakra/theme";
 import { ContextProvider } from "./context/ContextProvider";
-import './index.css';
+import "./index.css";
 
 const Application = () => {
-    // Registering Syncfusion license key
-    registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE_KEY);
+  const chains = [mainnet, avalancheFuji];
 
-    const chains = [mainnet, avalancheFuji];
+  // Wagmi client
+  const { provider } = configureChains(chains, [
+    walletConnectProvider({
+      projectId: process.env.REACT_APP_WEB3_MODAL_PROJECT_ID,
+    }),
+  ]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: modalConnectors({
+      projectId: process.env.REACT_APP_WEB3_MODAL_PROJECT_ID,
+      version: "2", // or "1"
+      appName: "web3Modal",
+      chains,
+    }),
+    provider,
+  });
 
-    // Wagmi client
-    const { provider } = configureChains(chains, [
-      walletConnectProvider({ projectId: process.env.REACT_APP_WEB3_MODAL_PROJECT_ID }),
-    ]);
-    const wagmiClient = createClient({
-      autoConnect: true,
-      connectors: modalConnectors({
-        projectId: process.env.REACT_APP_WEB3_MODAL_PROJECT_ID,
-        version: "2", // or "1"
-        appName: "web3Modal",
-        chains,
-      }),
-      provider,
-    });
+  // Web3Modal Ethereum Client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-    // Web3Modal Ethereum Client
-    const ethereumClient = new EthereumClient(wagmiClient, chains);
-
-    return (
-      <>
+  return (
+    <>
       <Web3Modal
         projectId={process.env.REACT_APP_WEB3_MODAL_PROJECT_ID}
         ethereumClient={ethereumClient}
         themeMode="light"
       />
-        <BrowserRouter>
-          <WagmiConfig client={wagmiClient}/>
-            <WagmiConfig client={wagmiClient}>
-              <ChakraProvider theme={theme}>
-                <ContextProvider>
-                  <App />
-                </ContextProvider>
-              </ChakraProvider>
-          </WagmiConfig>
-        </BrowserRouter>
-      </>
-    );
+      <BrowserRouter>
+        <WagmiConfig client={wagmiClient} />
+        <WagmiConfig client={wagmiClient}>
+          <ChakraProvider theme={theme}>
+            <ContextProvider>
+              <App />
+            </ContextProvider>
+          </ChakraProvider>
+        </WagmiConfig>
+      </BrowserRouter>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Application />
-  </React.StrictMode>,
+    <Application />
+  </React.StrictMode>
 );
