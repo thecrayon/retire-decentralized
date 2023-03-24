a3cabae03153b80a02eb1c1f15519388bc7c6f84
Billy Bund <billybund@Billys-MacBook-Pro-2.local>
2023-03-24 15:53:15 -0600

    added error messages to sign up wizard

--- frontend/src/components/Wizard/components/ConnectWallets.jsx
+++ frontend/src/components/Wizard/components/ConnectWallets.jsx
@@ -10,15 +10,47 @@ import {
   Th,
   Thead,
   Tr,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { formatAddress } from "../../../helpers";
 
+const pattern = "^0x[a-fA-F0-9]{40}$";
+
 const ConnectWallets = () => {
   const [wallet, setWallet] = useState("");
   const [connectedWallets, setConnectedWallets] = useState([]);
+  const toast = useToast();
+
+  const showErrorMessage = ({ title, description }) =>
+    toast({
+      title: title,
+      description: description,
+      status: "error",
+      duration: 5000,
+      isClosable: true,
+      position: "top",
+    });
 
   const onWalletAdd = () => {
+    if (!wallet || !wallet.trim()) return;
+
+    if (!wallet.match(pattern)) {
+      showErrorMessage({
+        title: "Invalid wallet address",
+        description: "Please enter a valid wallet address.",
+      });
+      return;
+    }
+
+    if (connectedWallets.includes(wallet)) {
+      showErrorMessage({
+        title: "Wallet already added",
+        description: "Please add a new wallet.",
+      });
+      return;
+    }
+
     const wallets = [...connectedWallets];
     wallets.push(String(wallet));
     setConnectedWallets([...wallets]);
@@ -67,7 +99,7 @@ const ConnectWallets = () => {
         justify="center"
       >
         {/* for some reason lg fontsize isn't applying same size font here as in rest of project */}
-        <Text fontWeight={700} fontSize="md">
+        <Text fontWeight={700} fontSize="lg">
           Add a wallet!
         </Text>
         <Input
